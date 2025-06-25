1fd4ba83c09d7fde669903993ec6a35682ef7d35
Jeremy Shafer <jeremy.j.shafer@gmail.com>
2025-06-25 18:21:51 -0400

    implemeting logout button

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,6 +1,15 @@
 $(document).ready(function () {
+
+  // if user is not logged in, redirect to index.html
   const usertoken = localStorage.getItem('usertoken');
   if (!usertoken) {
     window.location.href = 'index.html';
   }
+
+  // if logout button is clicked, kill the usertoken and redirect
+  $('#logout-btn').click(function () {
+    localStorage.removeItem('usertoken');
+    window.location.href = 'index.html';
+  });
+
 });
